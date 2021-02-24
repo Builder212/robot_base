46e5b50e2cf6f9a733def8ae7eef37afdce9c412
Builder212 <building2day@gmail.com>
2021-02-23 20:28:25 -0500

    Updated motor control code.

--- motor.py
+++ motor.py
@@ -5,32 +5,43 @@ class Motor:
     def __init__(self):
         try:
             self.usb = serial.Serial('/dev/ttyACM0', 9600)
+            sleep(2)
         except:
             print("Can't connect too arduino.")
 
     def forward(self):
-        self.usb.write(b'Forward')
+        self.usb.write(b'F')
 
     def reverse(self):
-        self.usb.write(b'Reverse')
+        self.usb.write(b'B')
 
     def stop(self):
-        self.usb.write(b'Stop')
+        self.usb.write(b'S')
 
     def right(self):
-        self.usb.write(b'Right')
+        self.usb.write(b'R')
 
     def left(self):
-        self.usb.write(b'Left')
+        self.usb.write(b'L')
+
+    def close(self):
+        self.usb.close()
 
 if __name__ == "__main__":
     motor = Motor()
     motor.forward()
+    print("1")
     sleep(2)
     motor.reverse()
+    print("2")
     sleep(2)
     motor.right()
+    print("3")
     sleep(2)
     motor.left()
+    print("4")
     sleep(2)
     motor.stop()
+    print("5")
+    sleep(2)
+    motor.close()
--- motor_control/motor_control.ino
+++ motor_control/motor_control.ino
@@ -6,7 +6,7 @@ AF_DCMotor motor_3(3, MOTOR12_64KHZ);
 AF_DCMotor motor_4(4, MOTOR12_64KHZ);
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
 
   motor_1.setSpeed(200);
   motor_2.setSpeed(200);
@@ -17,51 +17,32 @@ void setup() {
 void loop() {
   if (Serial.available()) {
     String command = Serial.readString();
-    if (command == "Forward") {
-      forward();
-    } else if (command == "Reverse") {
-      backward();
-    } else if (command == "Stop") {
-      stop();
-    } else if (command == "Right") {
-      right();
-    } else if (command == "Left") {
-      left();
+    if (command == "F") {
+      motor_1.run(FORWARD);
+      motor_2.run(FORWARD);
+      motor_3.run(FORWARD);
+      motor_4.run(FORWARD);
+    } else if (command == "B") {
+      motor_1.run(BACKWARD);
+      motor_2.run(BACKWARD);
+      motor_3.run(BACKWARD);
+      motor_4.run(BACKWARD);
+    } else if (command == "S") {
+      motor_1.run(RELEASE);
+      motor_2.run(RELEASE);
+      motor_3.run(RELEASE);
+      motor_4.run(RELEASE);
+    } else if (command == "R") {
+      motor_1.run(FORWARD);
+      motor_2.run(BACKWARD);
+      motor_3.run(FORWARD);
+      motor_4.run(BACKWARD);
+    } else if (command == "L") {
+      motor_1.run(BACKWARD);
+      motor_2.run(FORWARD);
+      motor_3.run(BACKWARD);
+      motor_4.run(FORWARD);
     }
   }
-}
-
-void forward() {
-  motor_1.run(FORWARD);
-  motor_2.run(FORWARD);
-  motor_3.run(FORWARD);
-  motor_4.run(FORWARD);
-}
-
-void backward() {
-  motor_1.run(BACKWARD);
-  motor_2.run(BACKWARD);
-  motor_3.run(BACKWARD);
-  motor_4.run(BACKWARD);
-}
-
-void stop() {
-  motor_1.run(RELEASE);
-  motor_2.run(RELEASE);
-  motor_3.run(RELEASE);
-  motor_4.run(RELEASE);
-}
-
-void right() {
-  motor_1.run(FORWARD);
-  motor_2.run(BACKWARD);
-  motor_3.run(FORWARD);
-  motor_4.run(BACKWARD);
-}
-
-void left() {
-  motor_1.run(BACKWARD);
-  motor_2.run(FORWARD);
-  motor_3.run(BACKWARD);
-  motor_4.run(FORWARD);
+  delay(10);
 }
